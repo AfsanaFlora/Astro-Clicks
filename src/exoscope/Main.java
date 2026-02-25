c87781563cd2463a2883af3f541b9305f3556c7e
Tuba Siddiqui <tubaashmeera@gmail.com>
2026-02-24 20:43:19 -0500

    C3A-3: the menu no longer crashes on wrong input

--- src/exoscope/Main.java
+++ src/exoscope/Main.java
@@ -1,47 +1,50 @@
 import model.Exoplanet;
 import java.util.*;
 import data.ExoplanetDataLoader;
-import java.io.*;
+import UI.MainView;
 
- /* public class Main {
 
-	public static void main(String[] args) {
-		
-		Exoplanet planet1 = new Exoplanet("Pluto","sun",20,60000,55,"telescope",15,1999);
-		Exoplanet planet2 = new Exoplanet("Jenn","sun",30,5000,20,"eyes",1999,2002);
-		Exoplanet planet3 = new Exoplanet("Tuba","sun2", 255, 10000, 30, "spaceship",20000,2025);
-		
-		List<Exoplanet> planets = new ArrayList<Exoplanet>();
-		planets.add(planet1);
-		planets.add(planet2);
-		planets.add(planet3);
-		
-		for (Exoplanet planet : planets) {
-			System.out.println(planet.getName());
-		}
+/* public class Main {
 
+public static void main(String[] args) {
+	
+	Exoplanet planet1 = new Exoplanet("Pluto","sun",20,60000,55,"telescope",15,1999);
+	Exoplanet planet2 = new Exoplanet("Jenn","sun",30,5000,20,"eyes",1999,2002);
+	Exoplanet planet3 = new Exoplanet("Tuba","sun2", 255, 10000, 30, "spaceship",20000,2025);
+	
+	List<Exoplanet> planets = new ArrayList<Exoplanet>();
+	planets.add(planet1);
+	planets.add(planet2);
+	planets.add(planet3);
+	
+	for (Exoplanet planet : planets) {
+		System.out.println(planet.getName());
 	}
 
+}
+
 } this was just an example */ 
-		
-		
+
 public class Main {
 
     public static void main(String[] args) {
 
-        // to create the data loader & then give it the csv file path
+        // to create the data loader and give it the CSV path
         ExoplanetDataLoader loader =
                 new ExoplanetDataLoader("src/exoscope/data/exoplanets.csv");
 
-        // to load all planets from the file
+        // to load all planets
         List<Exoplanet> planets = loader.loadExoplanets();
 
-        // this is to print how many planets were loaded
+        // to print how many planets were loaded
         System.out.println("Loaded " + planets.size() + " planets.");
 
-        // to print the first 5 planets to check if it worked
+        // to print first 5 planets 
         for (int i = 0; i < 5 && i < planets.size(); i++) {
             System.out.println(planets.get(i));
         }
+        
+        MainView view = new MainView(planets);
+        view.start();
     }
 }
